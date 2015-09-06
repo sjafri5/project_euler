b079120b846d30ccdfe1ad559aecdaa8f7f3c3d8
sjafri5 <raza.jafri.sy@gmail.com>
2015-09-05 22:17:09 -0500

    added not passing specs for FibSumFinder#intiialize

---
+++ spec/2_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe SumFinder do
+  before :context do
+    @sum_finder = FibSumFinder.new(1)
+  end
+  
+  describe 'initialize' do
+    it 'has @max_number as instance var'
+    it 'has @max_number as instance var'
+    it 'has @max_number as instance var'
+  end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,3 @@
 require_relative '../1'
- 
+require_relative '../2'
 require 'yaml'
